c71553fd90b35c52c1de858bf9028fabaa412165
Vicram <vicram.vijayakumar@gmail.com>
2024-04-23 19:10:25 -0700

    Add Group Button Fronted added

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,12 @@
 import UserInfo from "@/components/UserInfo";
-import AddGroupBtn from "@/components/AddGroupBtn";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="grid place-items-center h-screen -mt-24">
       <UserInfo />
-      <AddGroupBtn ref={'/groups'}/>
-
+      <Link href="/groups">
+        <button className="bg-slate-900 text-white px-6 py-2 rounded-md">Add Group</button> </Link>
     </div>
   );
 }
--- components/AddGroupBtn.jsx
+++ components/AddGroupBtn.jsx
@@ -1,20 +1,22 @@
 "use client";
 import React from 'react';
-import { motion } from 'framer-motion';
 
-const MyButton = React.forwardRef(({ onClick, href }, ref) => {
-  return (
-    <motion.a
-      href={href}
-      onClick={onClick}
-      ref={ref}
-      className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg shadow-xl items-center justify-center gap-4 cursor-pointer text-lg"
-      whileHover={{ scale: 1.1 }}
-      transition={{ type: "spring", stiffness: 300 }}
-    >
-      Add Group
-    </motion.a>
-  );
-});
+// const MyButton = React.forwardRef(({ onClick, href }, ref) => {
+//   return (
+//     <motion.a
+//       href={href}
+//       onClick={onClick}
+//       ref={ref}
+//       className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg shadow-xl items-center justify-center gap-4 cursor-pointer text-lg"
+//       whileHover={{ scale: 1.1 }}
+//       transition={{ type: "spring", stiffness: 300 }}
+//     >
+//       Add Group
+//     </motion.a>
+//   );
+// });
 
-export default MyButton;
+export default function MyButton() {
+  return <></>
+}
+
